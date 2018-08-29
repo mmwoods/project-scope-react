0c864ae8890d081f7c7bd1f479acbfc77813a2ed
Mitchell Woods <themitchwoods@gmail.com>
2018-08-30 00:32:12 +1000

    Updated to calculate data

--- src/components/AddScopeContent.js
+++ src/components/AddScopeContent.js
@@ -2,22 +2,22 @@ import React, { Component } from "react";
 
 class AddScopeContent extends Component {
   nameRef = React.createRef();
-  typeRef = React.createRef();
   designRef = React.createRef();
   codingRef = React.createRef();
-  totalRef = React.createRef();
-  costRef = React.createRef();
 
   createPage = event => {
     // 1. Stop the form from submitting
     event.preventDefault();
+    const codingHours = parseFloat(this.codingRef.current.value);
+    const designHours = parseFloat(this.designRef.current.value);
+    const total = codingHours + designHours;
     const page = {
       name: this.nameRef.current.value,
       type: this.typeRef.current.value,
-      design: parseFloat(this.designRef.current.value),
-      coding: parseFloat(this.codingRef.current.value),
-      total: parseFloat(this.totalRef.current.value),
-      cost: this.costRef.current.value
+      design: designHours,
+      coding: codingHours,
+      total: total,
+      cost: total * 160
     };
     this.props.addPage(page);
     // refresh the form
@@ -32,10 +32,6 @@ class AddScopeContent extends Component {
           type="text"
           placeholder="Page Name"
         />
-        <select name="type" ref={this.typeRef}>
-          <option value="design">Design</option>
-          <option value="coding">Coding</option>
-        </select>
         <input
           name="design"
           ref={this.designRef}
@@ -48,13 +44,6 @@ class AddScopeContent extends Component {
           type="text"
           placeholder="Coding Hours"
         />
-        <input
-          name="total"
-          ref={this.totalRef}
-          type="text"
-          placeholder="Total Hours"
-        />
-        <input name="cost" ref={this.costRef} type="text" placeholder="Cost" />
         <button type="submit">Add Page</button>
       </form>
     );
